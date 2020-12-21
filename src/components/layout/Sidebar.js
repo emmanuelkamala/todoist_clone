66fe31538d61c2ac403b78d4d624bab2da3e69c5
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-12-21 11:44:35 +0300

    Add content for the sidebar top

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,12 +1,32 @@
 import React from 'react';
+import { FaChevronDown, 
+         FaInbox, 
+         FaRegCalendarAlt, 
+         FaRegCalendar 
+       } from 'react-icons/fa';
 
 function Sidebar() {
   return (
     <div className="sidebar" data-testid="sidebar">
       <ul className="sidebar__generic">
-        <li>Inbox</li>
-        <li>Today</li>
-        <li>Next 7 Days</li>
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 Days</span>
+        </li>
       </ul>
     </div>
   )
