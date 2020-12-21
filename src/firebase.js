83e7cbc9ab9d46acd37694afe5a58296fc533132
emmanuelkamala <emmanuelkamala86@gmail.com>
2020-12-21 12:32:23 +0300

    Connect firestore with the app

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseConfig = firebase.initializeApp({
-
+  apiKey: "AIzaSyAw4KqL-xZ5tXJwt3kwbVGnyCY0jSOuPVE",
+  authDomain: "todoist-clone-d0c51.firebaseapp.com",
+  databaseURL: "https://todoist-clone-d0c51-default-rtdb.firebaseio.com",
+  projectId: "todoist-clone-d0c51",
+  storageBucket: "todoist-clone-d0c51.appspot.com",
+  messagingSenderId: "905116515747",
+  appId: "1:905116515747:web:07bd6022686d011fae6e16"
 });
 
 export { firebaseConfig as firebase };
